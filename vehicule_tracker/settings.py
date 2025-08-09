ad9c9c18fdefa1e1b3ae0e645f9a10beb4ac927d
haroldegbako <136116087+haroldegbako@users.noreply.github.com>
2025-08-09 23:30:51 +0100

    Update settings.py

--- vehicule_tracker/settings.py
+++ vehicule_tracker/settings.py
@@ -77,15 +77,19 @@ WSGI_APPLICATION = 'vehicule_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vehicle_tracking',
-        'USER': 'root',
-        'PASSWORD': 'harrysmith#2308',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+# Si DATABASE_URL existe (sur Railway), utilise PostgreSQL
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        default=os.environ['DATABASE_URL'],
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+    DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
